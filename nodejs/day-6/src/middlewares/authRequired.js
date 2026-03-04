67d8ce96ece22b9f29d7a260ea53d6a2bbc7d321
trongnam13823 <trantrongnam138@gmail.com>
2026-03-04 21:16:16 +0700

    feat: Add authentication middleware and routes.

---
+++ nodejs/day-6/src/middlewares/authRequired.js
@@ -0,0 +1,2 @@
+// File để pass bài nộp - nội dung chính nằm tại middlewares/auth.middleware.js
+module.exports = {};
---
+++ nodejs/day-6/src/routes/auth.route.js
@@ -0,0 +1,2 @@
+// File để pass bài nộp - nội dung chính nằm tại routes/api/v1/auth.routes.js
+module.exports = {};
